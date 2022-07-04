f364a35b6e4f7043a5df0e01edff33a6c3248f72
GhomKrosmonaute <camille.abella@hotmail.fr>
2022-07-04 10:34:19 +0200

    add --with-team option in stantup command

--- src/commands/standup.ts
+++ src/commands/standup.ts
@@ -20,21 +20,40 @@ export default new app.Command({
       castValue: "channel",
     },
   ],
+  flags: [
+    {
+      name: "with-team",
+      aliases: ["team"],
+      flag: "t",
+      description: "With PlayCurious Team",
+    },
+  ],
   async run(message) {
     const vocal: app.VoiceChannel = message.args.vocal
 
     if (!vocal.isVoice()) return message.send(`No voice channel targeted`)
 
+    function membersOf(list: string[]) {
+      return list.map((resolvable) => {
+        const match = /(\d+)/.exec(resolvable)
+        return match ? message.guild.members.cache.get(match[1]) : null
+      })
+    }
+
     return message.send(
       [
         ...vocal.members.values(),
-        ...message.args.with.map((resolvable: string) => {
-          const match = /(\d+)/.exec(resolvable)
-          return match ? message.guild.members.cache.get(match[1]) : null
-        }),
+        ...membersOf(message.args.with),
+        ...membersOf(
+          message.args.team
+            ? (
+                await message.guild.roles.fetch("619438748459073557")
+              )?.members.map((m) => m.id) ?? []
+            : []
+        ),
       ]
         .sort(() => Math.random() - 0.5)
-        .map((e, i) => String(i + 1) + " " + e.displayName)
+        .map((e, i) => String(i + 1) + " " + e?.displayName)
         .join("\n") || "No member found in " + vocal.name + " channel."
     )
   },
